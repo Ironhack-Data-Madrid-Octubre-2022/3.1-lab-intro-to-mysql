bc3e7aaa4ffad494d1962516bbae7cf2ebe98e2f
fjgarciaes <fj.garciaesurjc@gmail.com>
2022-10-31 18:38:55 +0100

    alfinalhiceelbonuscomobuenalumno

---
+++ your-code/update.sql
@@ -0,0 +1,15 @@
+update salesperson
+set store = 'Guadalajara'
+where idsalesperson = 2;
+
+update customers
+set Email = 'ppicasso@gmail.com'
+where idcustomers = 0;
+
+update customers
+set Email = 'psmanagement@nuevops.com'
+where idcustomers = 1;
+
+update customers
+set Email = 'lamerche@bigbrother.com'
+where idcustomers = 2;
