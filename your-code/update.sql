1258e65a9624c67b93da86ea04f701e8383135cf
Davidteje <davidtejesan@gmail.com>
2022-10-31 19:49:53 +0100

    Entrega

---
+++ your-code/update.sql
@@ -0,0 +1,4 @@
+UPDATE SALES_PERSONS SET STORE = 'Miami' WHERE ID = 4
+UPDATE CUSTOMERS SET `e-mail` = 'ppicasso@gmail.com' WHERE ID = 0;
+UPDATE CUSTOMERS SET `e-mail` = 'lamarr_hd@hollywood.com' WHERE ID = 1;
+UPDATE CUSTOMERS SET `e-mail` = 'hello@nasa.gov' WHERE ID = 2
