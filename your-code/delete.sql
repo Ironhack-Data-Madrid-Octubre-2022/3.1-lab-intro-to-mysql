1c2db09a1697b8e4fe1be8d464931de5b617edd2
rciomoreno <morenoalarconrocio@gmail.com>
2022-10-31 18:25:10 +0100

    [3.1-lab-intro-to-mysql] Rocío Moreno

---
+++ your-code/delete.sql
@@ -0,0 +1,1 @@
+DELETE FROM cars WHERE carid = 5;
---
+++ your-code/update.sql
@@ -0,0 +1,11 @@
+UPDATE customers 
+SET email = 'ppicasso@gmail.com'
+WHERE idcustomers = '10001'; 	
+
+UPDATE customers 
+SET email = 'carmengarcía@hollywood.com'
+WHERE idcustomers = 20001;
+
+UPDATE customers
+SET email = 'morenoalarconrocio@gmail.com'
+WHERE idcustomers = 30001;
