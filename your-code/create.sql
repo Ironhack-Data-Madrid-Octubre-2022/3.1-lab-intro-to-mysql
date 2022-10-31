ff5e40745221cd21c21364659469eccee2d32371
María <maria@MacBook-Pro-de-Maria.local>
2022-10-31 17:51:27 +0100

    soluciones

--- your-code/create.sql
+++ your-code/create.sql
@@ -1,4 +1,3 @@
-USE cars;
 -- MySQL Workbench Forward Engineering
 
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
@@ -22,13 +21,13 @@ USE `coches` ;
 -- Table `coches`.`Cars`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `coches`.`Cars` (
-  `VIN` VARCHAR(17) NOT NULL,
-  `Manufacturer` VARCHAR(45) NOT NULL,
-  `Model` VARCHAR(45) NOT NULL,
-  `Year` DATETIME NOT NULL,
+  `CarsID` INT NOT NULL,
+  `VIN` VARCHAR(17) NULL,
+  `Manufacturer` VARCHAR(45) NULL,
+  `Model` VARCHAR(45) NULL,
+  `Year` INT NULL,
   `Color` VARCHAR(45) NOT NULL,
-  `Salesperson_StaffID` INT NOT NULL,
-  PRIMARY KEY (`VIN`, `Salesperson_StaffID`))
+  PRIMARY KEY (`CarsID`))
 ENGINE = InnoDB;
 
 
@@ -36,17 +35,17 @@ ENGINE = InnoDB;
 -- Table `coches`.`Customers`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `coches`.`Customers` (
-  `Customer_ID` INT NOT NULL,
-  `Name` VARCHAR(45) NOT NULL,
+  `ID` INT NOT NULL,
+  `Customer_ID` INT NULL,
+  `Name` VARCHAR(45) NULL,
   `Phone` VARCHAR(45) NULL,
-  `Email` VARCHAR(45) NOT NULL,
+  `Email` VARCHAR(45) NULL,
   `Address` VARCHAR(45) NULL,
   `City` VARCHAR(45) NULL,
   `State` VARCHAR(45) NULL,
   `Country` VARCHAR(45) NULL,
   `Zip` VARCHAR(45) NULL,
-  `Salesperson_StaffID` INT NOT NULL,
-  PRIMARY KEY (`Customer_ID`, `Salesperson_StaffID`))
+  PRIMARY KEY (`ID`))
 ENGINE = InnoDB;
 
 
@@ -54,10 +53,11 @@ ENGINE = InnoDB;
 -- Table `coches`.`Salesperson`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `coches`.`Salesperson` (
-  `StaffID` INT NOT NULL,
+  `ID` INT NOT NULL,
+  `StaffID` INT NULL,
   `Name` VARCHAR(45) NULL,
   `Store` VARCHAR(45) NULL,
-  PRIMARY KEY (`StaffID`))
+  PRIMARY KEY (`ID`))
 ENGINE = InnoDB;
 
 
@@ -66,34 +66,28 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `coches`.`Invoices` (
   `invoice_ID` INT NOT NULL,
-  `Invoice_number` VARCHAR(45) NULL,
+  `Invoice_number` INT NULL,
   `Date` VARCHAR(45) NULL,
-  `Car` VARCHAR(45) NULL,
-  `Customer` VARCHAR(45) NULL,
-  `Salesperson` VARCHAR(45) NULL,
-  `Customers_Customer_ID` INT NOT NULL,
-  `Salesperson_StaffID` INT NOT NULL,
-  `Cars_VIN` VARCHAR(17) NOT NULL,
-  `Cars_Salesperson_StaffID` INT NOT NULL,
-  `Customers_Customer_ID1` INT NOT NULL,
-  `Customers_Salesperson_StaffID` INT NOT NULL,
-  PRIMARY KEY (`invoice_ID`, `Customers_Customer_ID`, `Salesperson_StaffID`, `Cars_VIN`, `Cars_Salesperson_StaffID`, `Customers_Customer_ID1`, `Customers_Salesperson_StaffID`),
-  INDEX `fk_Invoices_Salesperson1_idx` (`Salesperson_StaffID` ASC) VISIBLE,
-  INDEX `fk_Invoices_Cars1_idx` (`Cars_VIN` ASC, `Cars_Salesperson_StaffID` ASC) VISIBLE,
-  INDEX `fk_Invoices_Customers1_idx` (`Customers_Customer_ID1` ASC, `Customers_Salesperson_StaffID` ASC) VISIBLE,
+  `Cars_CarsID` INT NOT NULL,
+  `Customers_ID` INT NOT NULL,
+  `Salesperson_ID` INT NOT NULL,
+  PRIMARY KEY (`invoice_ID`, `Cars_CarsID`, `Customers_ID`, `Salesperson_ID`),
+  INDEX `fk_Invoices_Salesperson1_idx` (`Salesperson_ID` ASC) VISIBLE,
+  INDEX `fk_Invoices_Cars1_idx` (`Cars_CarsID` ASC) VISIBLE,
+  INDEX `fk_Invoices_Customers1_idx` (`Customers_ID` ASC) VISIBLE,
   CONSTRAINT `fk_Invoices_Salesperson1`
-    FOREIGN KEY (`Salesperson_StaffID`)
-    REFERENCES `coches`.`Salesperson` (`StaffID`)
+    FOREIGN KEY (`Salesperson_ID`)
+    REFERENCES `coches`.`Salesperson` (`ID`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Invoices_Cars1`
-    FOREIGN KEY (`Cars_VIN` , `Cars_Salesperson_StaffID`)
-    REFERENCES `coches`.`Cars` (`VIN` , `Salesperson_StaffID`)
+    FOREIGN KEY (`Cars_CarsID`)
+    REFERENCES `coches`.`Cars` (`CarsID`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Invoices_Customers1`
-    FOREIGN KEY (`Customers_Customer_ID1` , `Customers_Salesperson_StaffID`)
-    REFERENCES `coches`.`Customers` (`Customer_ID` , `Salesperson_StaffID`)
+    FOREIGN KEY (`Customers_ID`)
+    REFERENCES `coches`.`Customers` (`ID`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
